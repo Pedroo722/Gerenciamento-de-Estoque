690fa61d69e1836c63e8297a49fb9d2afd098486
Pedroo722 <pedrohenriquealexandre7@gmail.com>
2023-12-20 13:50:06 -0300

    roteamento

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,46 @@
 import React, { useEffect } from 'react';
-import HeaderHome from '../components/HeaderHome.jsx';
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { definirToken } from '../redux/loginSlice';
+import HeaderHome from '../components/HeaderHome.jsx';
 
-
-const Botoes = () => {  
+const Botoes = () => {
   return (
-      <div className="button-container">
-        <a className="button nova-venda" href="vendas">   Vendas   </a>
-        <a className="button estoque" href="estoque">Estoque</a>
-        <a className="button relatorio" href="relatorios">Relatórios</a>
-        <a className="button clientes" href="clientes">Clientes</a>
-      </div>    
+    <div className="button-container">
+      <Link className="button nova-venda" to="/vendas">
+        Vendas
+      </Link>
+      <Link className="button estoque" to="/estoque">
+        Estoque
+      </Link>
+      <Link className="button relatorio" to="/relatorios">
+        Relatórios
+      </Link>
+      <Link className="button clientes" to="/clientes">
+        Clientes
+      </Link>
+    </div>
   );
 };
-
-// Butão "Sobre", apenas para mobile e dispositivos pequenos
 
 const SobreMobile = () => {
-  return (
-    <a className="button-sobre-mobile" href="sobre">Sobre</a>
-  );
+  return <Link className="button-sobre-mobile" to="/sobre">Sobre</Link>;
 };
 
-
-// Componente principal que será exportado por defaut
-
-const Home = ({ history }) => {  
+const Home = () => {
   const token = useSelector((state) => state.token);
   const navigate = useNavigate();
 
   useEffect(() => {
     if (!token) {
-      console.log("Login")
+      console.log("Login");
       return navigate("/login");
     }
   }, [token]);
 
   return (
     <main>
-      <HeaderHome />      
+      <HeaderHome />
       <SobreMobile />
       <Botoes />
     </main>
