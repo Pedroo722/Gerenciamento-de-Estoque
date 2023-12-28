bd032caa8e0959e7ae7d15997100f287c56da667
Pedro Alexandre <132232273+Pedroo722@users.noreply.github.com>
2023-12-28 12:29:39 -0300

    login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -23,7 +23,7 @@ const Login = () => {
   };
 
   const logar = () => {
-    axios.post('https://back.pedrohenriq1921.repl.co/api/auth/local', formData)
+    axios.post('hhttps://ideacao-backend-8ea0b764c21a.herokuapp.com/api/auth/local', formData)
       .then((response) => {
         if (response.status === 200) {
           dispatch(definirToken(response.data.jwt));
